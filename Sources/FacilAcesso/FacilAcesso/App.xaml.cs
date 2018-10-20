d9e9d61b7825b6b33472366651715e20c62f5411
danskcarvalho <danskcarvalho@gmail.com>
2018-10-19 23:00:32 -0300

    v6

--- Sources/FacilAcesso/FacilAcesso/App.xaml.cs
+++ Sources/FacilAcesso/FacilAcesso/App.xaml.cs
@@ -11,7 +11,7 @@ namespace FacilAcesso
         {
             InitializeComponent();
 
-            var navigationPage = new NavigationPage(new CriarContaPage());
+            var navigationPage = new NavigationPage(new CartaoCreditoPage());
             navigationPage.BarTextColor = Color.White;
             navigationPage.BarBackgroundColor = Color.FromHex("#1D346B");
             MainPage = navigationPage;
